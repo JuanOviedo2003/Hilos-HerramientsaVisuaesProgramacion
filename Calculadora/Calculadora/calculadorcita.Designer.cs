ff5db665c4a4f992a17ceb82aaad01e48b6d6be8
Juan Oviedo <65878274+JuanOviedo2003@users.noreply.github.com>
2023-03-10 11:33:32 -0500

    Add files via upload

---
+++ Calculadora/Calculadora/calculadorcita.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Calculadora
+{
+    partial class calculadorcita
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(calculadorcita));
+            this.btnSumar_ = new System.Windows.Forms.Button();
+            this.btnRestar_ = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSumar_
+            // 
+            this.btnSumar_.Location = new System.Drawing.Point(96, 194);
+            this.btnSumar_.Name = "btnSumar_";
+            this.btnSumar_.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar_.TabIndex = 0;
+            this.btnSumar_.Text = "Sumar";
+            this.btnSumar_.UseVisualStyleBackColor = true;
+            // 
+            // btnRestar_
+            // 
+            this.btnRestar_.Location = new System.Drawing.Point(214, 194);
+            this.btnRestar_.Name = "btnRestar_";
+            this.btnRestar_.Size = new System.Drawing.Size(75, 23);
+            this.btnRestar_.TabIndex = 1;
+            this.btnRestar_.Text = "Restar";
+            this.btnRestar_.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(33, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 36);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CALCULADORCITA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label2.Location = new System.Drawing.Point(161, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "NUMERO 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label3.Location = new System.Drawing.Point(161, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "NUMERO 2";
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.ForeColor = System.Drawing.Color.Cornsilk;
+            this.resultado.Location = new System.Drawing.Point(180, 378);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(25, 33);
+            this.resultado.TabIndex = 9;
+            this.resultado.Text = "-";
+            this.resultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(96, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // calculadorcita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(378, 452);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnRestar_);
+            this.Controls.Add(this.btnSumar_);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "calculadorcita";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSumar_;
+        private System.Windows.Forms.Button btnRestar_;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculadora/Calculadora/calculadorcita.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class calculadorcita : Form
+    {
+        private Thread calculationThread;
+        public calculadorcita()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void equalsButton_Click(object sender, EventArgs e)
+        {
+            // Crea el objeto CalculationThread y lo inicia
+            calculationThread = new Thread(Calculate);
+            calculationThread.Start();
+        }
+
+        private void Calculate()
+        {
+            // Obtiene el valor del inputTextBox y realiza la operación matemática correspondiente
+            // Actualiza el resultado en el resultLabel
+        }
+    }
+}
